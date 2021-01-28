186640ee8060dbfca355298f838eed0e3a521a30
peppermint100 <44718131+peppermint100@users.noreply.github.com>
2021-01-28 19:54:46 +0900

    Delete client directory

--- client/app/src/main/java/com/example/projecttravelplanner/type/BaseResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.example.projecttravelplanner.type
-
-data class BaseResponse (
-    val msg: String,
-    val success: Boolean
-)
--- client/app/src/main/java/com/example/projecttravelplanner/type/auth/MeResponse.kt
+++
@@ -1,3 +0,0 @@
-package com.example.projecttravelplanner.type.auth
-
-data class MeResponse(val userId: String, val username: String, val email: String, val success: Boolean, val msg: String)
